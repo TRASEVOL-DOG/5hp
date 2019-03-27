d5b8ffee816c1559fe590df3d941ee5a470010ef
Remy <trasevol.dog@gmail.com>
2019-03-27 11:32:19 +0100

    made weapons golden

--- bullet.lua
+++ bullet.lua
@@ -223,8 +223,8 @@ function draw_bullet(s)
   local y = s.y + s.diff_y - 1
   
   if s.from == my_id then
-    pal(13,12)
-    pal(11,9)
+--    pal(13,12)
+--    pal(11,9)
   else
     pal(13,11)
     pal(11,8)
@@ -240,7 +240,8 @@ function draw_bullet(s)
     spr(237, x, y, 2, 1, s.angle)
   end
   
-  all_colors_to()
+  pal(13,13)
+  pal(11,11)
   
   if debug_mode then
     all_colors_to(14)
